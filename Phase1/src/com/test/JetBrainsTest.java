064156930fe4d3908be42981f43923918c2e3536
vshes <vshes.60@gmail.com>
2017-05-22 15:35:57 -0500

    initial commit

---
+++ Phase1/src/com/test/JetBrainsTest.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by shesh on 5/22/17.
+ */
+public class JetBrainsTest {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jet Brains!");
+    }
+}
