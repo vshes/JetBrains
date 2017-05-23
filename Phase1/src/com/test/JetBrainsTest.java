8d63624474f172922f1950d00aad22fdafb2690e
vshes <vshes.60@gmail.com>
2017-05-22 19:06:30 -0500

    Merge Conflic resolved Between Master and Phase1-alpha

--- Phase1/src/com/test/JetBrainsTest.java
+++ Phase1/src/com/test/JetBrainsTest.java
@@ -7,5 +7,7 @@ public class JetBrainsTest {
     public static void main(String[] args) {
         System.out.println("Hello from Jet Brains!");
         System.out.println("Create this commit to move master!");
+        FirstBetaClass fcb = new FirstBetaClass();
+        fcb.betaOne();
     }
 }
